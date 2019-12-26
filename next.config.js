f960ee640b8f6e808cd5360f49a2bef3fb1db0a1
scottie-schneider <scott@laclave.co>
2019-12-26 17:14:57 -0500

    fixing env variables in dev

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
+// For env variables in NextJS
+// step 1: require the dotenv library  
+require("dotenv").config();
 
-module.exports = {
-  webpack: (config) => {
+module.exports = {  
+  // step 2: declare the env variables in module exports
+  env: {
+    GOOGLE_MAPS: process.env.GOOGLE_MAPS
+  },
+  webpack: (config) => {    
     config.plugins.push(
       new SWPrecacheWebpackPlugin({
         verbose: true,
