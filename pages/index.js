c69bfac0a94a0f70544b2ffc29581266ff3e01b9
scottie-schneider <scott@laclave.co>
2019-12-28 22:13:03 -0500

    added protected route with auth

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import { authInitialProps } from "../lib/auth";
 import React, { useState, useEffect } from "react";
+import { getSessionFromClient, getSessionFromServer, redirectUser } from '../lib/auth'
 
+import Layout from '../components/Layout';
+import StoryList from '../components/StoryList'
 
-const Index = ({ classes, auth }) => {
+const Index = ({ classes, auth, stories }) => {
   useEffect(() => {
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker
@@ -21,6 +24,10 @@ const Index = ({ classes, auth }) => {
         // Auth User Page
         <div>
           Auth user page
+          <Layout title={'Hacker News Reader'} 
+          description={'A sample PWA built with React and Next.JS'}>
+            <StoryList stories={stories} />
+          </Layout>
         </div>
       ) : (
         // Splash Page (UnAuth Page)
@@ -33,8 +40,27 @@ const Index = ({ classes, auth }) => {
 }
 // Issues
 // 1 - use a hook to get the data for the stories 
-// 2 - ensure you're mounting the service worker (used component did mount)
+// 2 - DONE ensure you're mounting the service worker (used component did mount)
 
-Index.getInitialProps = authInitialProps();
+// Index.getInitialProps = authInitialProps();
+Index.getInitialProps = async function({req, res, query: { userId }}) {
+  // getting the auth and ensuring it's populated as props
+  const auth = req ? getSessionFromServer(req) : getSessionFromClient();
+  const currentPath = req ? req.url : window.location.pathname;
+  const user = auth.user;
+  const isAnonymous = !user;
+  // getting the stories from hacker news to seed our app
+  let stories
+  if (true && isAnonymous && currentPath !== "/signin") {
+    return redirectUser(res, "/signin");
+  }
+  try {
+    const req = await fetch(`https://node-hnapi.herokuapp.com/news?page=1`)
+    stories = await req.json()    
+  } catch(e){
+    stories = undefined
+  }
+  return { auth, userId, stories };
+};
 
 export default Index;
